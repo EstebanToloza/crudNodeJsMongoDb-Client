d78452d2effa985b722dbd9f4ba295eba03f9589
EstebanToloza <estebanisaiastoloza@gmail.com>
2020-11-17 21:12:20 -0300

    Repaso basico de contenido gestionado en el Backend

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+
+const app = express()
+
+app.listen(2000)
+app.use( express.static('public') )
+
+app.get('/:seccion', (req, res) =>{
+    const { seccion } = req.params
+    res.writeHead(200, { "Content-Type" : "text/html" })
+    res.end(`<h1>Hola desde le sección <u>${seccion}</u> en HTML</h1>`)
+})
